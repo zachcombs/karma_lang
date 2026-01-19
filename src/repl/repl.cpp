bd310a7879c387df2d87ea9b877151a328cac6b6
Zachary Combs <zcombs6@gatech.edu>
2026-01-19 12:23:50 -0500

    Added Read Eval Print Loop!

---
+++ src/repl/repl.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <iostream>
+
+#include <repl.hpp>
+#include <lexer/lexer.hpp>
+#include <token/token.hpp>
+
+namespace repl
+{
+    const std::string prompt = ">> ";
+
+    void Start(std::istream in, std::ostream out)
+    {
+        std::string line;
+
+        while (true)
+        {
+            out << prompt;
+            out.flush();
+
+            if (!std::getline(in, line))
+            {
+                return;
+            }
+
+            lexer::Lexer l(line);
+
+            for (token::Token tok = l.NextToken();
+                 tok.type != token::END_OF_FILE;
+                 tok = l.NextToken())
+            {
+                out << "Token{ type=" << tok.type << ", literal=\"" << tok.literal << "\" }\n";
+            }
+        }
+    }
+}
---
+++ src/repl/repl.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <istream>
+#include <ostream>
+
+namespace repl
+{
+    void Start(std::istream in, std::ostream out);
+}
